9b08a74a03867b2e3852c2220b84c9ea2c96e0c7
zd304 <568892758@qq.com>
2017-06-25 12:16:29 +0800

    preload test

--- js/script.js
+++ js/script.js
@@ -57,8 +57,8 @@ new WOW().init();
 //<![CDATA[
 $(window).load(function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(350).css({'overflow':'visible'});
+  $('#preloader').delay(5).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(5).css({'overflow':'visible'});
 })
 //]]>
 
